35b34cf9b7613c74f3b50f7cf87e0c0e19e4e505
Kusuma <thekoesoemo@gmail.com>
2024-03-18 06:15:15 +0700

    add files

--- src/Hash.hpp
+++ src/Hash.hpp
@@ -2,17 +2,21 @@
 // All rights reserved.
 // This software is distributed under the MIT License.
 
-
 #ifndef HASH_HPP
 #define HASH_HPP
 
-#include <string>
-#include <cstdio>
+#include "Crypto/Swifftx/SHA3.h"
+#include "Crypto/Swifftx/SWIFFTX.h"
 
 namespace SPHINXHash {
 
+    // Define the Hash() function interface
+    HashReturn Hash(int hashbitlen, const BitSequence *data, DataLength databitlen, 
+                    BitSequence *hashval);
+
+    // Define the existing hash function interface
     std::string SPHINX_256(const std::string& message);
 
 } // namespace SPHINXHash
 
-#endif // SPHINX_HASH_HPP
+#endif // HASH_HPP
